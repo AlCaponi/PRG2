49001905857235e5af302cd2c0e7b4268e4de115
Simon <Simon@Laptop-ASUSN76V>
2013-10-03 11:51:15 +0200

    ringpuffer

--- Test/src/Uebungen2/Ringbuffer.java
+++ Test/src/Uebungen2/Ringbuffer.java
@@ -4,10 +4,67 @@
  */
 package Uebungen2;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Simon
  */
 public class Ringbuffer {
-    
+
+    private ArrayList<Info> test;
+    int head;
+    int tail;
+    int capacity;
+
+    public Ringbuffer(int capacity) {
+
+        this.capacity = capacity;
+        head = 0;
+        tail = 0;
+        this.test = new ArrayList<>(capacity);
+    }
+
+    public void enqueue(Info x) {
+        // Fügt x am Ende in die Warteschlange ein, 
+        // falls die Warteschlange nicht voll ist; 
+        if (!isFull()) {
+
+            test.add(head, x);
+
+            if (head == test.size()) {
+                head = 0;
+            } else {
+                head++;
+            }
+        }
+    }
+
+    public Info dequeue() {
+        // Entfernt das erste Element aus der Warteschlange, 
+        // falls die Warteschlange nicht leer ist; 
+        Info element = null;
+        if (!isEmpty()) {
+
+
+            element = test.get(tail);
+            if (tail == test.size()) {
+                tail = 0;
+            } else {
+                tail++;
+            }
+
+        }
+        return element;
+    }
+
+    public boolean isEmpty() {
+        //liefert true genau dann, wenn die Warteschlange kein Element enthält 
+        return test.isEmpty();
+    }
+
+    public boolean isFull() {
+        //liefert true genau dann, wenn die Warteschlange voll ist 
+        return test.size() == this.capacity;
+    }
 }
